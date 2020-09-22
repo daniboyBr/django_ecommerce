612c8bd272e17c21f2df24f4af34036ad1f43ac8
Neander Wendel <neanderlink123@gmail.com>
2020-09-22 18:34:21 -0300

    Alteração do CSS para o ambiente publicado

--- api/settings.py
+++ api/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    Path.joinpath(BASE_DIR, 'static')
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
