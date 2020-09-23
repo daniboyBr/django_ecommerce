edee1449ff51e8cf35142ec838c0a6460bdd7ab3
Neander Wendel <neanderlink123@gmail.com>
2020-09-22 21:03:29 -0300

    Adição da cópia dos arquivos estáticos

--- api/settings.py
+++ api/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    Path.joinpath(BASE_DIR, 'static')
-)
+STATIC_ROOT = './static/'
+# STATICFILES_DIRS = (
+#     Path.joinpath(BASE_DIR, 'static')
+# )
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
